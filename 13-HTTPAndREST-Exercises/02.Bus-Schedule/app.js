065c2ebf2ef1fd35ed54de57538899df39b36b2e
Velin Iliev <73158924+VelinIliev@users.noreply.github.com>
2023-03-06 00:25:26 +0200

    refactor

--- 13-HTTPAndREST-Exercises/02.Bus-Schedule/app.js
+++ 13-HTTPAndREST-Exercises/02.Bus-Schedule/app.js
@@ -1,16 +1,42 @@
 function solve() {
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+    const info = document.querySelector('div#info span.info')
+    const url = `http://localhost:3030/jsonstore/bus/schedule`;
+
+    let nextStop = 'depot';
+    let stopName = ''
+
     function depart() {
-        // TODO: 
+        fetch(`${url}/${nextStop}`)
+        .then((response) => response.json())
+        .then((data) => {
+            stopName = data.name;
+            nextStop = data.next;
+            console.log(nextStop);
+            info.textContent = `Next stop ${data.name}`;
+            departBtn.disabled = true;
+            arriveBtn.disabled = false;
+            })
+        .catch(() => {
+            departBtn.disabled = true;
+            arriveBtn.disabled = true;
+            info.innerHTML = 'Error'
+        });
     }
 
-    async function arrive() {
-        // TODO:
+    function arrive() {
+        info.textContent = `Arriving at ${stopName}`;
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
     }
+   
 
     return {
         depart,
         arrive
     };
+   
 }
 
 let result = solve();
