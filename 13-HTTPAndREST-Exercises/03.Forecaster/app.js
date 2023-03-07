84954ae4630cfd224ce512a74ada437d5e842165
VelinIliev <veliniliev@gmail.com>
2023-03-07 11:30:20 +0200

    refactor

--- 13-HTTPAndREST-Exercises/03.Forecaster/app.js
+++ 13-HTTPAndREST-Exercises/03.Forecaster/app.js
@@ -7,69 +7,73 @@ function attachEvents() {
     const current = document.getElementById('current');
     const upcoming = document.getElementById('upcoming');
 
-
     let symbols = {
         "Sunny": '&#x2600',
         "Partly sunny": "&#x26C5",
         "Overcast": "&#x2601",
         "Rain": "&#x2614"
     }
-    let targetLocationCode = ''
+    let degrees = '&#176'
 
-    function todayForecast() {
-        fetch(`${url}today/${targetLocationCode}`)
-        .then((response) => response.json())
-        .then((data) => {
-            forecast.style.display = 'block';
-            current.innerHTML = `
-                <div class="label">Current conditions</div>
-                <span class="conrition symbol">${symbols[data.forecast.condition]}</span>
+    function displayTodayForecast(data) {
+        current.innerHTML = `
+            <div class="label">Current conditions</div>
+            <div class="forecasts">
+                <span class="condition symbol">${symbols[data.forecast.condition]}</span>
                 <span class="condition"> 
                     <span class="forecast-data">${data.name}</span>
-                    <span class="forecast-data">${data.forecast.low}&#176/${data.forecast.high}&#176</span>
+                    <span class="forecast-data">${data.forecast.low}${degrees}/${data.forecast.high}${degrees}</span>
                     <span class="forecast-data">${data.forecast.condition}</span>
-                </span>`
-            })
+                </span>
+            </div>
+            `
     }
-    function upcomingForecast() {
-        fetch(`${url}upcoming/${targetLocationCode}`)
-        .then((response) => response.json())
-        .then((data) => {
-            upcoming.innerHTML += `<div class="forcast-info">` 
-            data.forecast.forEach( day => {
-                upcoming.innerHTML += `
-                    <span class="upcoming">
+    
+    function displayUpcomingForecast(data) {
+        upcoming.innerHTML = '<div class="label">Three-day forecast</div>';
+        upcoming.innerHTML += `<div class="forecast-info"></div`; 
+        const forecastInfo = document.querySelector('.forecast-info');
+        data.forecast.forEach(day => {
+            forecastInfo.innerHTML += `
+                <span class="upcoming">
                     <span class="symbol">${symbols[day.condition]}</span>
-                    <span class="forecast-data">${day.low}&#176/${day.high}&#176</span>
+                    <span class="forecast-data">${day.low}${degrees}/${day.high}${degrees}</span>
                     <span class="forecast-data">${day.condition}</span>
-                `
-            });
-            upcoming.innerHTML += `</div">` 
-            })
+                </span>
+            `
+        });
+    }
+
+    async function getData() {
+        try {
+            let response = await fetch(`${url}locations/`);
+            let data = await response.json();
+            let currLocation = data.find(x => x.name.toLowerCase() === location.value.toLowerCase());
+
+            if(currLocation === undefined) {
+                forecastSection.innerHTML = "Error";
+            }
+
+            let code = currLocation.code;
+
+            forecast.style.display = 'block';
+
+            let todayForcastResponse = await fetch(`${url}today/${code}`);
+            let todayForcastData = await todayForcastResponse.json();
+            displayTodayForecast(todayForcastData);
+
+            let upcomingForecastResponse = await fetch(`${url}upcoming/${code}`);
+            let upcomingForecastData = await upcomingForecastResponse.json();
+            displayUpcomingForecast(upcomingForecastData);
+
+        } catch {
+            forecast.style.display = 'block';
+            forecast.innerHTML = 'Error';
+        }
         
     }
     
-    submitBtn.addEventListener('click', () => {
-        fetch(`${url}locations/`)
-        .then((response) => response.json())
-        .then((data) => {
-            let isFound = false
-            data.forEach(el => {
-                if ((el.name).toLowerCase() === (location.value).toLowerCase()) {
-                    targetLocationCode = el.code;
-                    isFound = true
-                    todayForecast();
-                    upcomingForecast();
-                }
-            });
-            if (!isFound) {
-                forecast.style.display = 'block';
-                forecast.innerHTML = 'Error'
-            }
-        })
-        
-        
-    })
+    submitBtn.addEventListener('click', getData)
 }
 
 attachEvents();
