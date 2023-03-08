3cf98b91f31db38e6f01189bd54f9432386141a5
Velin Iliev <73158924+VelinIliev@users.noreply.github.com>
2023-03-09 00:57:56 +0200

    update

--- 13-HTTPAndREST-Exercises/05.Messenger/app.js
+++ 13-HTTPAndREST-Exercises/05.Messenger/app.js
@@ -1,5 +1,54 @@
 function attachEvents() {
     // TODO:
+    const url = 'http://localhost:3030/jsonstore/messenger';
+    const sendBtn = document.getElementById('submit');
+    const refreshBtn = document.getElementById('refresh');
+    const messages = document.getElementById('messages');
+    const controls = document.getElementById('controls');
+    const name = document.querySelector("input[name='author']")
+    const content = document.querySelector("input[name='content']")
+
+    
+
+    function displayMessages(data) {
+        messages.innerHTML = '';
+        
+        Object.values(data).forEach((message, e) => {
+            if (e === Object.values(data).length - 1) {
+                messages.innerHTML += `${message.author}: ${message.content}`
+            } else {
+                messages.innerHTML += `${message.author}: ${message.content}\n`
+            }
+            
+        });
+    }
+    
+    function getPosts() {
+        fetch(url)
+        .then((response) => response.json())
+        .then((data) => displayMessages(data))
+        .catch((error) => console.log(error))
+    }
+
+    function sendMessage() {
+        fetch(url, {
+            method: "POST",
+            body: JSON.stringify({
+                "author": name.value,
+                "content": content.value,
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+        .catch((error) => console.log(error))
+        name.value = '';
+        content.value = '';
+    }
+
+    sendBtn.addEventListener('click', sendMessage)
+    refreshBtn.addEventListener('click', getPosts)
 }
 
 attachEvents();
+
--- 13-HTTPAndREST-Exercises/06.Phonebook/app.js
+++ 13-HTTPAndREST-Exercises/06.Phonebook/app.js
@@ -1,5 +1,63 @@
 function attachEvents() {
     // TODO:
+    const url = 'http://localhost:3030/jsonstore/phonebook';
+
+    const btnLoad = document.getElementById('btnLoad');
+    const btnCreate = document.getElementById('btnCreate');
+    const phoneBook = document.getElementById('phonebook');
+    const person = document.getElementById('person');
+    const phone = document.getElementById('phone')
+
+
+    function displayPhoneBook(data) {
+        phoneBook.innerHTML = ''
+        Object.values(data).forEach( entry => {
+            phoneBook.innerHTML += 
+            `<li id='${entry._id}'>${entry.person}: ${entry.phone}<button class="dltBtn">Delete</button></li>`
+        })
+    }
+
+    function getData() {
+        fetch(url)
+        .then((response) => response.json())
+        .then((data) => displayPhoneBook(data))
+        .catch((error) => console.log(error))
+    }
+
+    function deleteEntry(id) {
+        fetch(`${url}/${id}`, { method: 'DELETE' })
+        .then(response => response.json())
+        .then(getData())
+        .catch(error => console.log(error));
+    }
+
+    function createPerson() {
+        fetch(url, {
+            method: "POST",
+            body: JSON.stringify({
+                "person": person.value,
+                "phone": phone.value,
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+        .then(data => { getData() })
+        .catch((error) => console.log(error))
+        person.value = '';
+        phone.value = '';
+        
+    }
+
+    btnLoad.addEventListener('click', getData);
+    btnCreate.addEventListener('click', createPerson)
+    document.addEventListener('click', (e) => {
+        if (e.target.className === 'dltBtn'){
+            deleteEntry(e.target.parentNode.id)
+        }
+        
+    })
+
 }
 
 attachEvents();
